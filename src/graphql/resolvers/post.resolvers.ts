0c99363be235f6e0b7479d09e9ad451763a0a861
Cole Ogrodnick <patfan8@comcast.net>
2023-04-12 17:18:16 -0500

    refractor

--- src/graphql/resolvers/post.resolvers.ts
+++ src/graphql/resolvers/post.resolvers.ts
@@ -9,70 +9,79 @@ import {
 import { Post, User } from "../../models";
 import { HydratedDocument } from "mongoose";
 
-interface IPostResolver {
-  createPost: (args: { content: string }, req: Request) => PostRes;
-  getPosts: (args: GetPostsArgs) => Promise<GetPostsReturn | Error>;
-  getPostsByUser: (args: {
-    id: string;
-  }) => Promise<{ posts: IPost[]; count: number; user: IUser } | Error>;
-  updatePost: (
-    args: { postId: string; content: string },
-    req: Request
-  ) => PostRes;
-  deletePost: (args: { postId: string }, req: Request) => PostRes;
-}
+interface HydratedPost extends HydratedDocument<IPost> {}
 
-export const postResolver: IPostResolver = {
-  createPost: async ({ content }, { auth }) => {
-    if (!auth?.id) return new Error("Auth Required");
-    const post: HydratedDocument<IPost> = new Post({
-      content,
-      author: auth.id,
-    });
-    try {
-      await post.save();
-      return post.populate("author");
-    } catch (e: any) {
-      return new Error(e);
-    }
-  },
-  getPosts: async ({ page = 1, limit = 10, keyword = "" }) => {
-    const posts: HydratedDocument<IPost>[] = await Post.find({
-      content: { $regex: keyword },
-    })
-      .populate("author")
-      .limit(limit)
-      .skip((page - 1) * limit);
-
-    const count = posts.length;
-
-    return { posts, count, page, limit };
-  },
-  getPostsByUser: async ({ id = null }) => {
-    const user: HydratedDocument<IUser> | null = await User.findOne({
-      _id: id,
-    }).populate("posts");
-    if (!user) return new Error("No User found");
-
-    return { posts: user.posts, user, count: user.posts.length };
-  },
-  updatePost: async ({ postId, content }, { auth }) => {
-    if (!auth?.id) return new Error("Auth Required");
-    const post: HydratedDocument<IPost> | null = await Post.findOneAndUpdate(
-      { $and: [{ _id: postId }, { author: auth.id }] },
-      { content },
-      { new: true }
-    );
-    if (!post) return new Error("No post found");
+type CreatePost = (args: { content: string }, req: Request) => PostRes;
+const createPost: CreatePost = async ({ content }, { auth }) => {
+  if (!auth?.id) return new Error("Auth Required");
+  const post: HydratedPost = new Post({
+    content,
+    author: auth.id,
+  });
+  try {
     await post.save();
     return post.populate("author");
-  },
-  deletePost: async ({ postId }, { auth }) => {
-    if (!auth?.id) return new Error("Auth Required");
-    const post: HydratedDocument<IPost> | null = await Post.findOneAndDelete({
-      $and: [{ _id: postId }, { author: auth.id }],
-    });
-    if (!post) return new Error("No post found");
-    return post;
-  },
+  } catch (e: any) {
+    return new Error(e);
+  }
+};
+
+type GetPosts = (args: GetPostsArgs) => Promise<GetPostsReturn | Error>;
+const getPosts = async ({ page = 1, limit = 10, keyword = "" }) => {
+  const posts: HydratedPost[] = await Post.find({
+    content: { $regex: keyword },
+  })
+    .populate("author")
+    .limit(limit)
+    .skip((page - 1) * limit);
+
+  const count = posts.length;
+
+  return { posts, count, page, limit };
+};
+
+type GetPostsByUser = (args: {
+  id: string;
+}) => Promise<{ posts: IPost[]; count: number; user: IUser } | Error>;
+const getPostsByUser: GetPostsByUser = async ({ id = null }) => {
+  const user: HydratedDocument<IUser> | null = await User.findOne({
+    _id: id,
+  }).populate("posts");
+  if (!user) return new Error("No User found");
+
+  return { posts: user.posts, user, count: user.posts.length };
+};
+
+type UpdatePost = (
+  args: { postId: string; content: string },
+  req: Request
+) => PostRes;
+const updatePost: UpdatePost = async ({ postId, content }, { auth }) => {
+  if (!auth?.id) return new Error("Auth Required");
+  const post: HydratedPost | null = await Post.findOneAndUpdate(
+    { $and: [{ _id: postId }, { author: auth.id }] },
+    { content },
+    { new: true }
+  );
+  if (!post) return new Error("No post found");
+  await post.save();
+  return post.populate("author");
+};
+
+type DeletePost = (args: { postId: string }, req: Request) => PostRes;
+const deletePost: DeletePost = async ({ postId }, { auth }) => {
+  if (!auth?.id) return new Error("Auth Required");
+  const post: HydratedPost | null = await Post.findOneAndDelete({
+    $and: [{ _id: postId }, { author: auth.id }],
+  });
+  if (!post) return new Error("No post found");
+  return post;
+};
+
+export const postResolver = {
+  createPost,
+  getPosts,
+  getPostsByUser,
+  updatePost,
+  deletePost,
 };
